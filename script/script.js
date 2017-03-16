62360ddad65933adc8bc12d0c5785c8a90b38923
Maxim Marchenko <slayer.jk@mail.ru>
2017-03-16 16:25:33 +0600

    Slider JS code - done. Work on index-main(banners, feature-items, about-us - done).

--- script/script.js
+++ script/script.js
@@ -14,7 +14,21 @@ $(document).ready(function () {
   /*-------------------------------------------------------*/
 
   /*SLIDER*/
-  $.fn.sliderSwitcher = function () {
+  $.fn.sliderSwitcherManual = function () {
+    $('.slider__switch_1').on('click', function () {
+      $('body').removeClass('bg-2 bg-3').addClass('bg-1');
+    });
+    $('.slider__switch_2').on('click', function () {
+      $('body').removeClass('bg-1 bg-3').addClass('bg-2');
+    });
+    $('.slider__switch_3').on('click', function () {
+      $('body').removeClass('bg-1 bg-2').addClass('bg-3');
+    });
+  };
+  
+  $.fn.sliderSwitcherManual();
+  
+  $.fn.sliderSwitcherAuto = function () {
     var
       $currentInput = $('.slider__input:checked'),
       $nextInput = $currentInput.next(),
@@ -23,11 +37,17 @@ $(document).ready(function () {
     $currentInput.removeAttr('checked');
     if ($nextInput.prop('type') === 'radio') {
       $nextInput.prop('checked', 'checked');
+      if (!$('body').hasClass('bg-2')) {
+        $('body').removeClass('bg-1 bg-3').addClass('bg-2');
+      } else {
+        $('body').removeClass('bg-1 bg-2').addClass('bg-3');
+      }
     } else {
       $nextInput = $('.slider__input').first().prop('checked', 'checked');
+      $('body').removeClass('bg-2 bg-3').addClass('bg-1');
     }
   };
   
-  //setInterval($.fn.sliderSwitcher, 3000);
+  //setInterval($.fn.sliderSwitcherAuto, 3000);
 
 });
